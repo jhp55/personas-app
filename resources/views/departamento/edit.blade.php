775687e4eb413b95cadfa7b976204bb605e11f84
JAUN DAVID HERNANDEZ PERDOMO <juandavidhernandez515@hotmail.com>
2025-03-30 23:06:06 -0500

    departamento Edit

---
+++ resources/views/departamento/edit.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="es">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Edit de Departamentos</title>
+  </head>
+
+  <body>
+    <div class="container">
+        <h1>Edit de Departamentos</h1>
+
+        <form method="POST" action="{{ route('departamentos.update', ['departamento' => $departamento->depa_codi]) }}">
+            @method('put')
+            @csrf
+            <div class="mb-3">
+              <label for="codigo" class="form-label">Id</label>
+              <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id" disabled="disabled"
+              value="{{$departamento->depa_codi}}">
+              <div id="codigoHelp" class="form-text">departamento Id.</div>
+            </div>
+            <div class="mb-3">
+              <label for="name" class="form-label">departamento</label>
+              <input type="text" required class="form-control" id="name" placeholder="departamento name"
+                name="name" value="{{$departamento -> depa_nomb}}">
+            </div>
+
+            <label for="country">country:</label>
+            <select class="form-select" name="code" id="country" required>
+                <option selected disabled value="">Choose one...</option>
+                @foreach($paises as $pais)
+                    @if ($pais -> pais_codi == $departamento -> pais_codi)
+                        <option selected value="{{ $pais -> pais_codi }}">{{ $pais -> pais_nomb }}</option>
+                    @else
+                        <option value="{{ $pais -> pais_codi }}">{{ $pais -> pais_nomb }}</option>
+                    @endif
+                @endforeach
+            </select>
+            <div class="mt-3">
+                <button type="submit" class="btn btn-primary">updates</button>
+                <a href="{{route('departamentos.index')}}" class="btn btn-warning">Cancel</a>
+            </div>
+        </form>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
